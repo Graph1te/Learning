007f941dd5cf3a7f8de0780c791ca17ba7ce37ae
Egor <andersegor123@gmail.com>
2021-03-11 22:19:18 +0500

    Сделал первое приложение

--- first.js
+++ first.js
@@ -1,1 +1,23 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personaMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const filmName1 = prompt('Один из последних просмотренных фильмов?', ''),
+  filmRate1 = prompt('На сколько оцените его?', ''),
+  filmName2 = prompt('Один из последних просмотренных фильмов?', ''),
+  filmRate2 = prompt('На сколько оцените его?', '');
+
+
+personaMovieDB.movies[filmName1] = filmRate1;
+personaMovieDB.movies[filmName2] = filmRate2;
+
+
+console.log(personaMovieDB);
+
+
